84b6677bf06e0fd2111095ddf241863d85bbc0dc
bansi <bmpatel1701@gmail.com>
2021-03-24 17:05:34 +0530

    registration

--- base/com/vo/registration_vo.py
+++ base/com/vo/registration_vo.py
@@ -3,20 +3,18 @@ from base import database
 
 class faceVO(database.Model):
     __tablename__ = 'face_database'
-    face_id = database.Column('face_id', database.Integer, primary_key=True, autoincrement=True)
-    face_name = database.Column('face_name', database.String(100))
-    face_email_id = database.Column('face_email_id', database.String(100))
-    face_password= database.Column('face_password', database.String(100), nullable=False)
-    face_txt=database.column('face_txt',database.string(100),nullable=False,default="default.txt")
+    user_id = database.Column('user_id', database.Integer, primary_key=True, autoincrement=True)
+    user_name = database.Column('user_name', database.String(100))
+    user_email_id = database.Column('user_email_id', database.String(100))
+    user_password= database.Column('user_password', database.String(100), nullable=False)
 
 
     def as_dict(self):
         return {
-            'face_id': self.face_id,
-            'face_name': self.face_name,
-            'face_email_id': self.face_email_id,
-            'face_password':self.face.password,
-            'face_txt':self.face_txt
+            'user_id': self.user_id,
+            'user_name': self.user_name,
+            'user_email_id': self.user_email_id,
+            'user_password':self.user_password
 
         }
 
